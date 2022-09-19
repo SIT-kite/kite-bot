2da0820c5e78577c992f0642298262cf027be06f
zzq <i@zhangzqs.cn>
2022-09-20 00:29:41 +0800

    Fix: 修复兔小巢实体解析错误

--- txc_entity.py
+++ txc_entity.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 import enum
+import json
 from typing import *
 
 
@@ -36,17 +37,21 @@ class ReplyEntity:
 @dataclass
 class HookEntity:
     id: str
-    type: EventType
-    created_at: datetime
+    # type: EventType
+    # created_at: datetime
     payload: Union[PostEntity, ReplyEntity]
 
     @staticmethod
     def fromDict(dic: Dict[str, Any]):
         if dic['type'] == 'post.created':
-            payload = PostEntity.fromDict(dic['payload'])
+            payload = PostEntity.fromDict(dic['payload']['post'])
         if dic['type'] == 'post.updated':
-            payload = PostEntity.fromDict(dic['payload'])
+            payload = PostEntity.fromDict(dic['payload']['post'])
         return HookEntity(
             id=dic['id'],
             payload=payload
         )
+if __name__ == '__main__':
+    a = b'{"id":"36601f779a56dfde511045ade752b837","type":"post.created","payload":{"post":{"id":"166360302733603118","product_id":377648,"has_admin_reply":false,"avatar_url":"http:\\/\\/thirdqq.qlogo.cn\\/g?b=oidb&k=YPhqzYbLDibZ1GWLymWa5Uw&s=100&t=1598893744","nick_name":"zzq","content":"\\u5367\\u69fd\\u5367\\u69fd\\u5367\\u69fd","openid":"","user_id":4,"user":{"id":4,"openid":"B68364D35797AF97B9C5889755D67FC8","nickname":"zzq","avatar":"http:\\/\\/thirdqq.qlogo.cn\\/g?b=oidb&k=YPhqzYbLDibZ1GWLymWa5Uw&s=100&t=1598893744","os":" ","client":" ","network_type":null,"posts_count_last_3_months":0,"posts_count":0,"mark_as_good_count":"","mark_as_spam_count":0,"is_spam":false,"is_admin":null},"is_admin":true,"is_top":false,"is_good":false,"is_spam":false,"is_todo":false,"is_locked":false,"is_hidden":false,"is_notice":false,"is_liked":false,"is_abuse":false,"reply_count":0,"like_count":null,"images_count":0,"created_at":"2022-09-19 23:57:07","time":"\\u73b0\\u5728","updated_at":"2022-09-19 23:57:07","last_reply_at":null,"images":[],"replies":[],"extra":[],"faq_id":0,"type":2,"replies_all":[],"post_url":"https:\\/\\/support.qq.com\\/product\\/377648\\/post\\/166360302733603118?","categories":[]}},"created_at":"2022-09-19T23:57:07+08:00","retry_count":0}'.decode('utf-8')
+    b = HookEntity.fromDict(json.loads(a))
+    print(b)
