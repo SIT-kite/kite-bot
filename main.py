19b88b84e43dc8179c32481eefc741bb0ed2f9fa
zzq <i@zhangzqs.cn>
2022-09-19 18:58:30 +0800

    Update: 更换机器人框架

--- main.py
+++ main.py
@@ -1,71 +1,19 @@
+import telebot
+from telebot import apihelper
 
-import asyncio
-import logging
+apihelper.proxy = {
+    'http':'http://127.0.0.1:7890',
+    'https':'http://127.0.0.1:7890',
+}
 
-from telegram import __version__ as TG_VER
-import telegram
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+bot = telebot.TeleBot("", parse_mode=None)
 
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-def main():
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder()\
-        .proxy_url('socks5://127.0.0.1:7890')\
-        .token("")\
-        .build()
-
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(
-        filters.TEXT & ~filters.COMMAND, echo))
-
-
-    # Run the bot until the user presses Ctrl-C
-    # application.run_polling()
-    application.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+bot.infinity_polling()
