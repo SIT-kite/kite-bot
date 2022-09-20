f80b9bb9b813dddc951f7259affbe3fabe7c35a2
zzq <i@zhangzqs.cn>
2022-09-20 11:01:01 +0800

    Add: 尝试创建一个按钮

--- bot.py
+++ bot.py
@@ -19,6 +19,22 @@ async def send_current_time(message: Message):
     await bot.reply_to(message, bm.build_current_time())
 
 
+@bot.message_handler(commands=['button'])
+async def send_btn_message(message: Message):
+    await bot.send_message(
+        chat_id=current_config.chat_id,
+        text='button test',
+        reply_markup=InlineKeyboardMarkup(
+            keyboard=[[
+                InlineKeyboardButton(
+                    text='打开上应小风筝首页',
+                    url='https://kite.sunnysab.cn'
+                )
+            ]]
+        ),
+    )
+
+
 @bot.message_handler(func=lambda m: True)
 async def echo_all(message: Message):
     print(message)
@@ -30,6 +46,7 @@ async def set_bot_commands():
     await bot.set_my_commands(
         commands=[
             BotCommand('now', '获取当前时间'),
+            BotCommand('button', '测试一个按钮'),
         ]
     )
 
