e11f0159ed5ad5f689e6588a79cfa68e9e141f57
zzq <i@zhangzqs.cn>
2022-09-20 10:17:27 +0800

    Update: 优化机器人推送消息格式

--- api.py
+++ api.py
@@ -1,24 +1,36 @@
 import json
 from aiohttp.web import *
 import bot
-import txc_entity
+from txc_entity import TxcHookEntity, POST_CREATED
 
 app = Application()
+
+
+def build_txc_post_message(message: TxcHookEntity):
+    return f"""
+创建时间: {message.created_at}
+消息类型: {message.type}
+消息内容: {message.payload['post'].content}
+""".strip()
+
 
 async def handle(request: Request):
     bs = await request.content.read()
     content = bs.decode('utf-8')
-    e = txc_entity.HookEntity.fromDict(json.loads(content))
-    payload: txc_entity.PostEntity = e.payload
-    await bot.send_text_message(payload.content)
+    txc_message: TxcHookEntity = TxcHookEntity.from_dict(json.loads(content))
+    if txc_message.type == POST_CREATED:
+        await bot.send_text_message(build_txc_post_message(txc_message))
     return Response(status=200)
+
 
 app.add_routes([
     post('/feedback_hook', handle)
 ])
 
+
 def run_web_server(loop):
     run_app(app, loop=loop)
+
 
 __all__ = [
     'run_web_server'
